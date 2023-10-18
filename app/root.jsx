4b2f74f48cef1085ea2ef7862fe6f0e97f09c4d4
xandro2021 <xandrocastro2021@gmail.com>
2023-10-18 11:46:38 -0600

    Create root and add meta, links and styles

---
+++ app/root.jsx
@@ -0,0 +1,66 @@
+import {
+  Meta,
+  Links,
+} from '@remix-run/react';
+import styles from './styles/index.css';
+
+export function meta() {
+  return (
+    [
+      { charset: 'utf-8' },
+      { title: 'GuitarLA - Remix' },
+      { viewport: 'width=device-width,initial-scale=1' }
+    ]
+  )
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: 'https://necolas.github.io/normalize.css/8.0.1/normalize.css'
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossOrigin: 'true'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap'
+    },
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+export default function app() {
+
+  return (
+    <Document>
+      <h1>Hola Mundo</h1>
+    </Document>
+  )
+}
+
+function Document({ children }) {
+
+  return (
+    <html lang="es">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
