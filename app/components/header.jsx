97c78a3a065aa7fca39f1b316b8c14da0b12eed9
xandro2021 <xandrocastro2021@gmail.com>
2023-10-18 20:01:30 -0600

    Give styles to hint the current location in the navbar

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -16,18 +16,22 @@ function Header() {
         <div className="navegacion">
           <Link
             to="/"
+            className={location.pathname === '/' ? 'active' : ''}
           >Inicio</Link>
 
           <Link
             to="/nosotros"
+            className={location.pathname === '/nosotros' ? 'active' : ''}
           >Nosotros</Link>
 
           <Link
             to="/tienda"
+            className={location.pathname === '/tienda' ? 'active' : ''}
           >Tienda</Link>
 
           <Link
             to="/blog"
+            className={location.pathname === '/blog' ? 'active' : ''}
           >Blog</Link>
 
         </div>
