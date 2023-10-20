35a4cec6e311e22b80fe824887669442b01a6da9
xandro2021 <xandrocastro2021@gmail.com>
2023-10-19 19:24:34 -0600

    Create the section regarding the products (tienda)

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,14 +1,30 @@
+import { useLoaderData } from '@remix-run/react';
 import { getGuitarras } from '~/models/guitarras.server';
+import Guitarra from '~/components/guitarra';
 
 export async function loader() {
   const guitarras = await getGuitarras();
-  return guitarras;
+  return guitarras.data;
 }
 
 function Tienda() {
+  const guitarras = useLoaderData();
 
   return (
-    <h1>Compra en nuestra tienda</h1>
+    <main className="contenedor">
+      <h2 className="heading">Nuestra Coleccion</h2>
+
+      {guitarras?.length && (
+        <div className="guitarras-grid">
+          {guitarras.map(guitarra => (
+            <Guitarra
+              key={guitarra?.id}
+              guitarra={guitarra?.attributes}
+            />
+          ))}
+        </div>
+      )}
+    </main>
   );
 }
 
