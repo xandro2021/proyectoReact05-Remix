99f223832476bb57ed3538affbaf5c46f7513fc9
xandro2021 <xandrocastro2021@gmail.com>
2023-10-19 20:43:11 -0600

    Style the store section (tienda.jsx)

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,25 @@
 import { useLoaderData } from '@remix-run/react';
 import { getGuitarras } from '~/models/guitarras.server';
 import Guitarra from '~/components/guitarra';
+import styles from '~/styles/guitarras.css';
+
+export function meta() {
+  return [
+    {
+      title: 'GuitarLA - Tienda de Guitarras',
+      description: 'GuitarLA - Nuestra colección de guitarras'
+    }
+  ]
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
 
 export async function loader() {
   const guitarras = await getGuitarras();
