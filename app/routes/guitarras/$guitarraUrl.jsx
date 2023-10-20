e1ab4afcbf8a1053d444941a1252d1112d3d33d2
xandro2021 <xandrocastro2021@gmail.com>
2023-10-20 15:06:37 -0600

    Create dynamic route and style it for each product

---
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -0,0 +1,46 @@
+import { useLoaderData } from '@remix-run/react';
+import { getGuitarra } from '~/models/guitarras.server';
+import styles from '~/styles/guitarras.css';
+
+// Una vez el loader este disponible, la funcion meta tiene la info en data
+export function meta({ data }) {
+
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    }
+  ]
+}
+
+export async function loader({ params }) {
+  const { guitarraUrl } = params;
+
+  const guitarra = await getGuitarra(guitarraUrl);
+
+  return guitarra;
+}
+
+function Guitarra() {
+  const guitarra = useLoaderData();
+  const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
+
+  return (
+    <main className="contenedor guitarra">
+
+      <img className="imagen" alt={`Imagen de la guitarra ${nombre}`} src={imagen.data.attributes.url} />
+
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">${precio}</p>
+      </div>
+
+    </main>
+  );
+}
+
+export default Guitarra;
