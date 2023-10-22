ce69ffa5cc1aebfae6bc5e2370f32e9ff37432d4
xandro2021 <xandrocastro2021@gmail.com>
2023-10-22 17:44:34 -0600

    display the store guitars in index

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,7 +1,45 @@
+import { useLoaderData } from '@remix-run/react';
+import { getGuitarras } from '~/models/guitarras.server';
+import { getPosts } from '~/models/posts.server';
+import ListadoGuitarras from '~/components/listado-guitarras';
+import stylesGuitarras from '~/styles/guitarras.css';
+
+export function meta() {
+
+}
+
+export function links() {
+  return [
+    {
+      rel: 'stylesheet',
+      href: stylesGuitarras
+    }
+  ]
+}
+
+export async function loader() {
+  const [guitarras, posts] = await Promise.all([
+    getGuitarras(),
+    getPosts()
+  ]);
+
+  return {
+    guitarras: guitarras.data,
+    posts: posts.data
+  }
+}
+
 function Index() {
+  const { guitarras, posts } = useLoaderData();
 
   return (
-    <div>Hola desde Index Cabroooness</div>
+    <>
+      <main className="contenedor">
+        <ListadoGuitarras
+          guitarras={guitarras}
+        />
+      </main>
+    </>
   );
 }
 
