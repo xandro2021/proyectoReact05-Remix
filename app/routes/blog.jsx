205f817b268b57e0b0cfae4d0f5ee07e9d5be941
xandro2021 <xandrocastro2021@gmail.com>
2023-10-23 15:56:14 -0600

    display posts in index

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from '@remix-run/react';
 import { getPosts } from '~/models/posts.server.js';
-import Post from '~/components/post';
+import ListadoPosts from '~/components/listado-posts';
 import styles from '~/styles/blog.css';
 
 export function meta() {
@@ -30,17 +30,9 @@ function Blog() {
 
   return (
     <main className="contenedor">
-
-      <h2 className="heading">Blog</h2>
-      <div className="blog">
-        {posts.map(post => (
-          <Post
-            key={post.id}
-            post={post.attributes}
-          />
-        ))}
-      </div>
-
+      <ListadoPosts
+        posts={posts}
+      />
     </main>
   );
 }
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -2,7 +2,9 @@ import { useLoaderData } from '@remix-run/react';
 import { getGuitarras } from '~/models/guitarras.server';
 import { getPosts } from '~/models/posts.server';
 import ListadoGuitarras from '~/components/listado-guitarras';
+import ListadoPosts from '~/components/listado-posts';
 import stylesGuitarras from '~/styles/guitarras.css';
+import stylesPosts from '~/styles/blog.css';
 
 export function meta() {
 
@@ -13,6 +15,10 @@ export function links() {
     {
       rel: 'stylesheet',
       href: stylesGuitarras
+    },
+    {
+      rel: 'stylesheet',
+      href: stylesPosts
     }
   ]
 }
@@ -39,6 +45,12 @@ function Index() {
           guitarras={guitarras}
         />
       </main>
+
+      <section className="contenedor">
+        <ListadoPosts
+          posts={posts}
+        />
+      </section>
     </>
   );
 }
