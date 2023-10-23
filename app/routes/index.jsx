c036c6cbbdd085ffc1b80cb3b847f524507c74bd
xandro2021 <xandrocastro2021@gmail.com>
2023-10-23 16:59:35 -0600

    display curso.jsx in index and style it

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,10 +1,13 @@
 import { useLoaderData } from '@remix-run/react';
 import { getGuitarras } from '~/models/guitarras.server';
 import { getPosts } from '~/models/posts.server';
+import { getCurso } from '~/models/curso.server';
 import ListadoGuitarras from '~/components/listado-guitarras';
 import ListadoPosts from '~/components/listado-posts';
+import Curso from '~/components/curso';
 import stylesGuitarras from '~/styles/guitarras.css';
 import stylesPosts from '~/styles/blog.css';
+import stylesCurso from '~/styles/curso.css';
 
 export function meta() {
 
@@ -19,24 +22,30 @@ export function links() {
     {
       rel: 'stylesheet',
       href: stylesPosts
-    }
+    },
+    {
+      rel: 'stylesheet',
+      href: stylesCurso
+    },
   ]
 }
 
 export async function loader() {
-  const [guitarras, posts] = await Promise.all([
+  const [guitarras, posts, curso] = await Promise.all([
     getGuitarras(),
-    getPosts()
+    getPosts(),
+    getCurso()
   ]);
 
   return {
     guitarras: guitarras.data,
-    posts: posts.data
+    posts: posts.data,
+    curso: curso.data,
   }
 }
 
 function Index() {
-  const { guitarras, posts } = useLoaderData();
+  const { guitarras, posts, curso } = useLoaderData();
 
   return (
     <>
@@ -45,6 +54,10 @@ function Index() {
           guitarras={guitarras}
         />
       </main>
+
+      <Curso
+        curso={curso.attributes}
+      />
 
       <section className="contenedor">
         <ListadoPosts
